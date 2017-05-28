897d8f794d2bc46df9d150c9ae643f6c39befcf5
Daisuke Inoue <hibariyashiki@gmail.com>
2017-05-28 19:31:33 +0900

    oYunoki

---
+++ oYunoki.rb
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+class Yunoki
+	def initialize(input)
+		@input = input
+		@table = File.join(File.dirname(__FILE__), './table.dat')
+
+		@parent_array = Array.new
+
+		File.open(@table) do |file|
+			file.each do |line|
+				@parent_array << line.split(' ')
+			end
+		end
+
+	end
+
+	def target_array
+		@target_array = @parent_array.select do |child_array|
+			child_array.include? @input
+		end.flatten
+	end
+
+	def output
+		if self.target_array == nil or self.target_array.index(@input) == nil then
+			nil
+		else
+			@in_index = self.target_array.index(@input)
+			@out_index = 
+				if @in_index == self.target_array.size - 1 then 0
+				else @in_index + 1
+				end
+			@target_array[@out_index]
+		end
+	end
+end
+
+	input = ARGV[0] || STDIN.gets.strip
+	print Yunoki.new(input).output || input
